1c3c5425961ed26b854bf7aad3830943165713dd
GarrettPoore <garrettjpoore@gmail.com>
2017-07-14 20:55:01 -0400

    Added clock and adjusting time lengths functionality

---
+++ clock.js
@@ -0,0 +1,79 @@
+var work_time = 60;
+var break_time = 5;
+var running = false;
+var mode = "work"; //work or break
+
+var time = work_time * 60;
+
+function startClock() {
+  running = true;
+}
+
+function stopClock() {
+  running = false;
+}
+
+function updateWorkTime(num) {
+  work_time = num;
+  if (mode == "work") {
+    updateTime(work_time * 60);
+  }
+}
+
+function updateBreakTime(num) {
+  break_time = num;
+  if (mode == "break") {
+    updateTime(break_time * 60);
+  }
+}
+
+function updateTime(num) {
+  time = num;
+  updateClock();
+}
+
+//Either tick down the clock or switch it to the alternative
+function tickTock() {
+  if (running) {
+    if (time > 0) {
+      time -= 1;
+      updateClock();
+    } else if (mode == "work") {
+      mode = "break";
+      time = break_time * 60;
+      updateClock();
+    } else {
+      mode = "work";
+      time = work_time * 60;
+      updateClock();
+    }
+  }
+}
+
+function updateClock() {
+  $("#clock").text(getFormattedTime());
+}
+
+//Take the current time and format it as hh:mm:ss
+function getFormattedTime() {
+  var workingTime = time;
+
+  var hours = "";
+  if (workingTime >= 3600) {
+    hours = makeTwoDigits(Math.floor(time/3600));
+    hours = hours + ":";
+    workingTime = time % 3600;
+  }
+
+  var mins = makeTwoDigits(Math.floor(workingTime/60));
+  var secs = makeTwoDigits(workingTime % 60);
+  return hours + mins + ":" + secs;
+}
+
+//Take a number and make sure it is 2 digits long
+function makeTwoDigits(num) {
+  if (num < 10) {
+    return "0" + num;
+  }
+  return num;
+}
--- scripts.js
+++ scripts.js
@@ -1,3 +1,87 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  updateClock();
+  setInterval(tickTock, 1000);
+  $("#work_length").val(work_time);
+  $("#break_length").val(break_time);
 
+  //Start or stop the clock
+  $("#clock").click(function() {
+    if (running) {
+      running = false;
+    } else {
+      running = true;
+    }
+  });
+
+  //When the break_length text is changed
+  $("#break_length").on("input propertychange paste", function(event) {
+    var val = event.target.value;
+    if (!running) {
+      if (!isNaN(val)) {
+        //Is a number
+        val = Math.floor(val);
+        //Validation between 1 and 999
+        if (val === 0) {
+          val = 1;
+        } else if (val/1000 > 1) {
+          val = break_time;
+          alert("Please enter a number between 1 and 999");
+        }
+        updateBreakTime(val);
+      }
+    }
+    $("#break_length").val(break_time);
+  });
+
+  //When the work_length text is changed
+  $("#work_length").on("input propertychange paste", function(event) {
+    var val = event.target.value;
+    if (!running) {
+      if (!isNaN(val)) {
+        //Is a number
+        val = Math.floor(val);
+        //Validation between 1 and 999
+        if (val === 0) {
+          val = 1;
+        } else if (val/1000 > 1) {
+          val = work_time;
+          alert("Please enter a number between 1 and 999");
+        }
+        updateWorkTime(val);
+      }
+    }
+    $("#work_length").val(work_time);
+  });
+
+  //Click either increase button
+  $(".increase_time").click(function(event) {
+    var val = event.target.value;
+    if (!running) {
+      if (val == "work") {
+        updateWorkTime(work_time + 1);
+        $("#work_length").val(work_time);
+      } else {
+        updateBreakTime(break_time + 1);
+        $("#break_length").val(break_time);
+      }
+    }
+  });
+
+  //Click either decrease button
+  $(".decrease_time").click(function(event) {
+    var val = event.target.value;
+    if (!running) {
+      if (val == "work") {
+        if (work_time > 1) {
+          updateWorkTime(work_time - 1);
+          $("#work_length").val(work_time);
+        }
+      } else {
+        if (break_time > 1) {
+          updateBreakTime(break_time - 1);
+          $("#break_length").val(break_time);
+        }
+      }
+    }
+  });
 });
