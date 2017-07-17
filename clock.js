cac8ba5c1a29ce43a4eea0fe28edd61961782d96
GarrettPoore <garrettjpoore@gmail.com>
2017-07-16 20:00:37 -0400

    Added progress circle and finished up style changes

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@ var running = false;
 var mode = "work"; //work or break
 
 var time = work_time * 60;
+var total_time = time;
 
 function startClock() {
   running = true;
@@ -29,6 +30,7 @@ function updateBreakTime(num) {
 
 function updateTime(num) {
   time = num;
+  total_time = time;
   updateClock();
 }
 
@@ -41,10 +43,16 @@ function tickTock() {
     } else if (mode == "work") {
       mode = "break";
       time = break_time * 60;
+      total_time = time;
+      $("#mode").text("Break");
+      $("#clock_background").css("background", "rgb(80, 0, 0)");
       updateClock();
     } else {
       mode = "work";
       time = work_time * 60;
+      total_time = time;
+      $("#mode").text("Work");
+      $("#clock_background").css("background", "rgb(8, 84, 2)");
       updateClock();
     }
   }
@@ -52,13 +60,14 @@ function tickTock() {
 
 function updateClock() {
   $("#clock").text(getFormattedTime());
+  updateClockBackground();
 }
 
 //Take the current time and format it as hh:mm:ss
 function getFormattedTime() {
   var workingTime = time;
-
   var hours = "";
+
   if (workingTime >= 3600) {
     hours = makeTwoDigits(Math.floor(time/3600));
     hours = hours + ":";
@@ -76,4 +85,18 @@ function makeTwoDigits(num) {
     return "0" + num;
   }
   return num;
+}
+
+//The circle will fill up as the work timer ticks down
+//It then drains during the break
+function updateClockBackground() {
+  var remainingTime = total_time - time;
+  var percentage = remainingTime/total_time;
+  var size = 15 * percentage;
+
+  //Reverse the background to have it go the opposite direction
+  if (mode == "break") {
+    size = 15 - size;
+  }
+  $("#clock_background").css("clip", "rect(0, 15em, " + size + "em, 0)");
 }
